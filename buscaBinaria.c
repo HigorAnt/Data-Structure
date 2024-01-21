4e1ab12c85f30451206f6bd0b18361b9c7f8a168
HigorAnt <higor.ant02@gmail.com>
2024-01-21 08:14:48 -0300

    Changes

--- buscaBinaria.c
+++ buscaBinaria.c
@@ -3,40 +3,34 @@
 int v[100];
 
 int main(void) {
-	
-	for(int i=0;i<100;i++)
-	{
-		v[i] = i + 1;
-	}
 
 	int num = 100;
-
 	int comparacoes = 0;
-
 	int continuar = 1;
-
 	int ini = 0;
 	int fim = 99;
 
-	while(continuar == 1)
-	{
+	for(int i=0;i<100;i++){
+		v[i] = i + 1;
+	}
+
+	while(continuar == 1){
 		comparacoes++;
-		int pivo = (ini + fim) / 2;
-		if(v[pivo] == num){
+		int pivo=(ini + fim)/2;
+		if(v[pivo]==num){
 			printf("Elemento encontrado\n");
 			continuar = 0;
 		}
 		else{
-			if(num > v[pivo]){
-				ini = pivo + 1;
+			if(num>v[pivo]){
+				ini=pivo+1;
 			}
 			else{
-				fim = pivo - 1;
+				fim=pivo-1;
 			}
 		}
 
-		if(fim < ini)
-		{
+		if(fim<ini){
 			printf("Elemento não encontrado\n");
 			continuar = 0;
 		}
--- buscaSequencial.c
+++ buscaSequencial.c
@@ -3,13 +3,11 @@
 int v[10] = {4,8,2,56,34,7,98,1,25,10};
 
 int main(void) {
-	
+
 	int num = 10;
 
-	for(int i = 0; i < 10; i++)
-	{
-		if(v[i] == num)
-		{
+	for(int i = 0; i < 10; i++){
+		if(v[i] == num){
 			printf("Elemento encontrado na posição: %d\n",i);
 		}
 	}
